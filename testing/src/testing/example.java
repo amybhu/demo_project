96c7d00166ae9ee58fd31cc5eb9056619aa3cf9d
AM20171370 <AM20171370@10.218.136.32>
2020-12-09 16:44:15 +0530

    second commit

--- testing/src/testing/example.java
+++ testing/src/testing/example.java
@@ -1,23 +1,41 @@
 package testing;
 
 public class example {
-	public int cube(int x)
-	{
-		return x*x*x;
-	}
-	public int count(String word)
-	{
-		int count=0;
-		for(int i=0;i<word.length();i++)
-		{
-			if(word.charAt(i)=='a'||word.charAt(i)=='A')
-			{
-				count++;
-			}
-		}
-		return count;
+	public static void main(String[] args) {
+	      int a = 11;
+	      int b = 6;
+	      double c = 7.3;
+	      double d = 9.4;
+	      int result1 = minFunction(a, b);
+	      
+	      // same function name with different parameters
+	      double result2 = minFunction(c, d);
+	      System.out.println("Minimum Value = " + result1);
+	      System.out.println("Minimum Value = " + result2);
+	   }
+
+	   // for integer
+	   public static int minFunction(int n1, int n2) {
+	      int min;
+	      if (n1 > n2)
+	         min = n2;
+	      else
+	         min = n1;
+
+	      return min; 
+	   }
+	   
+	   // for double
+	   public static double minFunction(double n1, double n2) {
+	     double min;
+	      if (n1 > n2)
+	         min = n2;
+	      else
+	         min = n1;
+
+	      return min; 
+	   }
 	}
 
-}
 
 
